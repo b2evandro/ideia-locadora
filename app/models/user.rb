67139423556bb9bfe02960a4f38830a791ce8461
b2evandro <evandro.sl@aluno.ifsc.edu.br>
2022-02-28 17:00:37 -0300

    feat: validações cadastro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   # O nome do outro modelo é PLURALIZADO ao declarar uma has_many associação.
   # Quando usado com o belongs_to tem uma relação de um para muitos BIDIRECIONAL.
   has_many :favorites
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, on: :create
+  validates :gender, length: { maximum: 5 }
 end
